6a088c8c2833a543eb8b1810299c6662962bc33e
bjavohir2197 <bjavohir2197@gmail.com>
2026-04-18 15:21:46 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,33 @@
+# 2 ta ro'yxatni birlashtiring
+ro'yxat1 = [1, 2, 3, 4, 5]
+ro'yxat2 = ['a', 'b', 'c', 'd', 'e']
+
+# Ro'yxatlarni birlashtiring
+birlashtirilgan_ro'yxat = ro'yxat1 + ro'yxat2
+
+# Natija
+print(birlashtirilgan_ro'yxat)
+```
+
+```python
+# 2 ta ro'yxatni birlashtiring
+ro'yxat1 = [1, 2, 3, 4, 5]
+ro'yxat2 = ['a', 'b', 'c', 'd', 'e']
+
+# Ro'yxatlarni birlashtiring
+birlashtirilgan_ro'yxat = list(ro'yxat1 + ro'yxat2)
+
+# Natija
+print(birlashtirilgan_ro'yxat)
+```
+
+```python
+# 2 ta ro'yxatni birlashtiring
+ro'yxat1 = [1, 2, 3, 4, 5]
+ro'yxat2 = ['a', 'b', 'c', 'd', 'e']
+
+# Ro'yxatlarni birlashtiring
+birlashtirilgan_ro'yxat = ro'yxat1 + ro'yxat2
+
+# Natija
+print(birlashtirilgan_ro'yxat)
